6fb1b000074e63b78ea5535c85d0d6157bca89f9
DiegoMariottini <diegomariottini90@gmail.com>
2016-04-06 12:54:19 +0200

    ppp

--- Prova8/WebContent/codiceJS/index.js
+++ Prova8/WebContent/codiceJS/index.js
@@ -1,15 +1,15 @@
 function SubmitSenzaSpazi(testo){
 	var query=testo;
-	if(query.charAt(0)==""){
-		alert("Scrivi quello che mi vuoi chiedere")
-	}else{
-	
-	if(query.charAt(0)==" "){
-		alert("Elimina gli spazi iniziali")
-	
-	
-	}else{
-		document.campoDiRicerca.submit();
+	while(query.charAt(0)==" "){
+		var lunghezza=query.lenght;
+		var queryModificata=query.substring(2,lunghezza);
+		query=queryModificata;
+
 	}
+
+	if(query.charAt(0)==""){
+		alert("Scrivi quello che mi vuoi chiedere");
+	}else{	
+		document.campoDiRicerca.submit();
 	}
 }
