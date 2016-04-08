43eea59e6c665e0a0dee4bd5343c91e888b624d6
DiegoMariottini <diegomariottini90@gmail.com>
2016-04-08 12:04:37 +0200

    prova finale

--- Prova8/src/init/InitIndex.java
+++ Prova8/src/init/InitIndex.java
@@ -1,6 +1,14 @@
 package init;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
 
 import lucene.Indexer;
 import lucene.IndexerSpell;
@@ -15,37 +23,60 @@ public class InitIndex {
 	static String dataDir = PatterAssolut.getDataDir();
 
 	public static void main(String[] args) throws IOException {
-		
-		
-		
-		
-		try{
-			IndexerSpell indexerSpell = new IndexerSpell(indexDizionario);
-			indexerSpell.createIndex(dizionario, new TextFileFilter());
-			indexerSpell.close();
 
+		List<String> nomi=new ArrayList<String>(creaListaCartelle() );
+		//Lo devo iterare per ogni cartella
+			try{
+				IndexerSpell indexerSpell = new IndexerSpell(indexDizionario);
+				indexerSpell.createIndex(dizionario, new TextFileFilter());
+				indexerSpell.close();
+				for(String nome:nomi){
+					String nomeCartella=nome;
+					File f = new File (dataDir+nomeCartella);
+					if(f.exists()){
+						Indexer indexer = new Indexer(indexDir);
+						int numIndexed;
+						long startTime = System.currentTimeMillis();
+						numIndexed = indexer.createIndex(dataDir+nomeCartella, new TextFileFilter());
+						long endTime = System.currentTimeMillis();
+						indexer.close();
+						System.out.println(numIndexed+" File indexed, time taken: "
+								+(endTime-startTime)+" ms");	
+						System.out.println(f.listFiles().length);
+					}
+				}
+				}catch(IOException e)	{
+				e.printStackTrace();
+			}
+			}
+	
 
-			Indexer indexer = new Indexer(indexDir);
-			int numIndexed;
-			long startTime = System.currentTimeMillis();
+	public static List<String> creaListaCartelle(){
+
+		LinkedList<String> listaNomi = new LinkedList<String>();
+		
+		String fileNomiCaretelle=PatterAssolut.getDoveSiTrovaIlFileNomiCartelle();
+		
+		Scanner readerNomiCartelle;
+		try {
+			readerNomiCartelle = new Scanner(new FileReader(fileNomiCaretelle));
+			while(readerNomiCartelle.hasNext())	{
+				String persona= readerNomiCartelle.nextLine();
+				String personaSenzaSpazi=persona.replaceAll(" ","_");
+				personaSenzaSpazi = personaSenzaSpazi.replace("\"", "");
+				char asc27=27;
+				personaSenzaSpazi = personaSenzaSpazi.replace(""+asc27, "");
+				personaSenzaSpazi=personaSenzaSpazi.substring(0,(personaSenzaSpazi.length()));
+				listaNomi.add(personaSenzaSpazi);
+			}
+			readerNomiCartelle.close();
 			
-			//Lodevo iterare per ogni cartella
-			
-			String nomeCartella="Alessandro_Cialfi/";
-			numIndexed = indexer.createIndex(dataDir+nomeCartella, new TextFileFilter());
-			
-			//fineIndicizzazione
-			
-			
-			
-			long endTime = System.currentTimeMillis();
-			indexer.close();
-			System.out.println(numIndexed+" File indexed, time taken: "
-					+(endTime-startTime)+" ms");	
-		}catch(IOException e)	{
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+				
+		return listaNomi;
 	}
 
 }
--- Prova8/src/lucene/PatterAssolut.java
+++ Prova8/src/lucene/PatterAssolut.java
@@ -4,9 +4,11 @@ public class PatterAssolut {
 
 	//lanciare le pagine in localhost:3000
 	static String home="/home/diegomariottini/"; 
+	
 	static String indexDir= home+"git/progetto_agiw_due/Prova8/Index/";
-	static String doveSiTrovaIlFileNomiCartelle="";
+	static String doveSiTrovaIlFileNomiCartelle="/home/diegomariottini/Scrivania/Dati/name1.txt";
 	static String dataDir=home+"Scrivania/Dati/";
+	
 	//static String dataDirPrimo=PatterAssolut.getDataDir() + "Alessandro_Cialfi/";
 	static String dirDizionario=home+"git/progetto_agiw_due/Prova8/dizionario/";
 	static String dizionario =home+"git/progetto_agiw_due/Prova8/dizionario/Dizionario - nomi/";
