c697046af6a87cecb16d8cd2d47f09408d0c45a4
DiegoMariottini <diegomariottini90@gmail.com>
2016-04-08 10:13:58 +0200

    come prima

--- Prova8/src/lucene/PatterAssolut.java
+++ Prova8/src/lucene/PatterAssolut.java
@@ -5,6 +5,7 @@ public class PatterAssolut {
 	//lanciare le pagine in localhost:3000
 	static String home="/home/diegomariottini/"; 
 	static String indexDir= home+"git/progetto_agiw_due/Prova8/Index/";
+	static String doveSiTrovaIlFileNomiCartelle="";
 	static String dataDir=home+"Scrivania/Dati/";
 	//static String dataDirPrimo=PatterAssolut.getDataDir() + "Alessandro_Cialfi/";
 	static String dirDizionario=home+"git/progetto_agiw_due/Prova8/dizionario/";
@@ -31,6 +32,9 @@ public static String getIndexDizionario() {
 		return dataDirPrimo;
 	}
 	*/
+	public static String getDoveSiTrovaIlFileNomiCartelle() {
+		return doveSiTrovaIlFileNomiCartelle;
+	}
 	public static String getDirDizionario() {
 		return dirDizionario;
 	}
